b29957d0753c5ced78ccc738b6c84711467e88b5
Felipe Nader <fsnader@gmail.com>
2022-05-25 23:32:19 -0300

    Stonewall exercise

---
+++ src/AlgorithmsPracticeTests/Codility/Lesson7_StacksAndQueues/StoneWallTests.cs
@@ -0,0 +1,19 @@
+﻿using AlgorithmsPractice.Codility.Lesson7_StacksAndQueues;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace AlgorithmsPracticeTests.Codility.Lesson7_StacksAndQueues
+{
+    public class StoneWallTests
+    {
+        [Theory]
+        [InlineData(new [] {8, 8, 5, 7, 9, 8, 7, 4, 8}, 7)]
+        public void ShouldWork(int[] skyline, int expected)
+        {
+            var result = StoneWall.GetNumberOfBlocks(skyline);
+            Assert.Equal(expected, result);
+        }
+    }
+}
