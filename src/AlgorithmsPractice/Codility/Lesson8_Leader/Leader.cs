af062696c8d3246db49d0f5d81d37b9d83c74b9f
Felipe Nader <fsnader@gmail.com>
2022-06-08 23:04:57 -0300

    Lesson 8 - Leader

---
+++ src/AlgorithmsPractice/Codility/Lesson8_Leader/Leader.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AlgorithmsPractice.Codility.Lesson8_Leader
+{
+    public static class Leader
+    {
+        public static int SlowLeader(int[] array) // O(N^2)
+        {
+            var length = array.Length;
+
+            for (var i = 0; i < length; i++) // O(N)
+            {
+                var candidate = array[i];
+                var count = 0;
+
+                for (var j = 0; j < length; j++) // O(N)
+                {
+                    if (array[j] == candidate)
+                    {
+                        count++;
+                    }
+                }
+
+                if (count > length / 2)
+                {
+                    return candidate;
+                }
+            }
+
+            return -1;
+        }
+
+        public static int FastLeader(int[] array) // N*LOG(N)
+        {
+            var length = array.Length;
+            var count = 0;
+            var orderedArray = array.OrderBy(i => i).ToArray(); // LOG (N)
+            var candidate = orderedArray[length / 2];
+
+
+            for (int i = 0; i < length; i++) // O(N)
+            {
+                if (array[i] == candidate)
+                {
+                    count++;
+                }
+            }
+
+            if (count > length / 2)
+            {
+                return candidate;
+            }
+
+            return -1;
+        }
+
+        public static int GoldenLeader(int[] array) // O(2*n)
+        {
+            var length = array.Length;
+            var size = 0;
+
+            var value = array[0];
+
+            for (int i = 0; i < length; i++) // O(n)
+            {
+                if (size == 0)
+                {
+                    size++;
+                    value = array[i];
+                }
+                else
+                {
+                    if (value != array[i]) size--;
+                    else size++;
+                }
+            }
+            var candidate = -1;
+
+            if (size > 0) candidate = value;
+
+            var count = 0;
+
+            for(int i = 0; i < length; i++) // O(n)
+            {
+                if (array[i] == candidate)
+                {
+                    count++;
+                }
+            }
+
+            if (count > length / 2)
+            {
+                return candidate;
+            }
+
+            return -1;
+        }
+    }
+}
---
+++ src/AlgorithmsPracticeTests/Codility/Lesson8_Leader/LeaderTests.cs
@@ -0,0 +1,38 @@
+﻿using AlgorithmsPractice.Codility.Lesson8_Leader;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace AlgorithmsPracticeTests.Codility.Lesson8_Leader
+{
+    public class LeaderTests
+    {
+        [Theory]
+        [InlineData(new[] { 6, 8, 4, 6, 8, 6, 6 }, 6)]
+        [InlineData(new[] { 3, 4, 3, 3, 2, 3, -1, 3, 3}, 3)]
+        public void SlowShouldWork(int[] array, int expectedResult)
+        {
+            var result = Leader.SlowLeader(array);
+            Assert.Equal(expectedResult, result);
+        }
+
+        [Theory]
+        [InlineData(new[] { 6, 8, 4, 6, 8, 6, 6 }, 6)]
+        [InlineData(new[] { 3, 4, 3, 3, 2, 3, -1, 3, 3 }, 3)]
+        public void FastShouldWork(int[] array, int expectedResult)
+        {
+            var result = Leader.FastLeader(array);
+            Assert.Equal(expectedResult, result);
+        }
+
+        [Theory]
+        [InlineData(new[] { 6, 8, 4, 6, 8, 6, 6 }, 6)]
+        [InlineData(new[] { 3, 4, 3, 3, 2, 3, -1, 3, 3 }, 3)]
+        public void GoldenShouldWork(int[] array, int expectedResult)
+        {
+            var result = Leader.GoldenLeader(array);
+            Assert.Equal(expectedResult, result);
+        }
+    }
+}
